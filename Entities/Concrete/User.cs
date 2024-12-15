a888295c1540cc262df904c9546e9622c6bba020
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-12-15 21:57:41 +0300

    Assets added

---
+++ Entities/Concrete/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+	public class User
+	{
+		public virtual ICollection<RefreshToken> RefreshTokens { get; set; } = default!;
+
+	}
+}
