d6d25d6f5c7dd17d9b06be8d3b25dc6f0903726e
tugceazizeo@gmail.com <tugceazizeo@gmail.com>
2024-12-23 21:28:10 +0300

    Crud transactions added

---
+++ DataAccess/Concrete/InMemory/InMemorySurveyResultDal.cs
@@ -0,0 +1,48 @@
+﻿using Core.DataAccess;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+	public class InMemorySurveyResultDal : IEntityRepository<SurveyResult, int>
+	{
+
+		private readonly HashSet<SurveyResult> _surveyResults = new();
+		public void Add(SurveyResult entity)
+		{
+			entity.CreatedAt = DateTime.Now;
+			_surveyResults.Add(entity);	
+
+
+
+		}
+
+		public void Delete(SurveyResult entity)
+		{
+			entity.DeleteAt = DateTime.Now;
+
+		}
+
+		public SurveyResult GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<SurveyResult> GetList()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(SurveyResult entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+	
+		
---
+++ DataAccess/Concrete/InMemory/InMemoryUserDal.cs
@@ -0,0 +1,39 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+	public class InMemoryUserDal : IUserDal
+	{
+		private readonly HashSet<User>_users;
+		public void Add(IUserDal entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Delete(IUserDal entity)
+		{
+			throw new NotImplementedException();
+		}
+
+		public IUserDal GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<IUserDal> GetList()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Update(IUserDal entity)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
