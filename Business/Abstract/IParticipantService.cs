815e78e17d7577263c53cf73f793bd68c4e1f347
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-01-04 13:00:09 +0300

    Business layer created. Abstract and concrete classes added

---
+++ Business/Abstract/IParticipantService.cs
@@ -0,0 +1,14 @@
+﻿
+
+using Entities.Concrete;
+
+namespace Business.Abstract
+{
+	public  interface IParticipantService
+	{
+
+		public Participant AddParticipant(Participant participant);
+
+		public IList<Participant> GetList();	
+	}
+}
---
+++ Business/Concrete/QuestionManager.cs
@@ -0,0 +1,8 @@
+﻿
+
+namespace Business.Concrete
+{
+	public class QuestionManager
+	{
+	}
+}
