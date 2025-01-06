53333ee6b8a5b7da631300fe96d52bbf387d6a0c
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-01-06 23:51:59 +0300

    New file added

---
+++ WebAPI/Controllers/ParticipantsController.cs
@@ -0,0 +1,42 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ParticipantsController : ControllerBase
+	{
+		private readonly IParticipantService _participantService;
+
+		public ParticipantsController()
+		{
+			_participantService = ServiceRegistration.ParticipantService;
+		}	
+
+
+
+		[HttpGet] //GET http://localhost:5161/api/participants
+		public ICollection<Participant> Getlist() 
+		{
+			IList<Participant>participantList = _participantService.GetList();
+			return participantList;
+		}
+		[HttpPost] //POST http://localhost:5161/api/participants
+		public Participant Add(Participant addParticipantRequest) 
+		{
+			Participant addParticipant =_participantService.Add(addParticipantRequest);
+		    return addParticipant;
+		}
+
+
+
+
+
+	}
+}
---
+++ WebAPI/ServiceRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+
+namespace WebAPI
+{
+	public static class ServiceRegistration
+	{
+		public static IParticipantDal ParticipantDal = new InMemoryParticipantDal();
+		public static IParticipantService ParticipantService = new ParticipantManager(ParticipantDal);
+
+
+	}
+}
