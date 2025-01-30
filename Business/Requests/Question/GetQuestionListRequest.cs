66399fa33e9ab7f78da8ad46400293271112766e
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-01-30 22:18:12 +0300

    Request class added

--- Business/Requests/Question/GetQuestionListRequest.cs
+++ Business/Requests/Question/GetQuestionListRequest.cs
@@ -2,5 +2,11 @@
 {
 	public class GetQuestionListRequest
 	{
+
+        public int? SurveyId { get; set; }
+		public int PageNumber { get; set; } = 1; // Sayfalama için sayfa numarası
+		public int PageSize { get; set; } = 10; // Sayfa başına kaç soru gösterilecek
+		public string? SearchKeyword { get; set; }//sorular içinde arama yapmak için
+
 	}
 }
