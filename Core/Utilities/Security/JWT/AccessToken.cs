494d0bff67fc7e3bd9deb378c6df8f34d40be030
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-02-13 19:24:10 +0300

    JWT

---
+++ Core/Utilities/Security/JWT/AccessToken.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace Core.Utilities.Security.JWT
+{
+	public  class AccessToken
+	{
+        public string Token { get; set; }
+        public DateTime ExpirationTime { get; set; }
+
+
+    }
+}
