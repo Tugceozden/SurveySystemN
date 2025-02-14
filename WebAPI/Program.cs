073078f0f34b8080c5d9652b4e56200881aaf68f
tugceazizeo@gmail.com <tugceeazizee@gmail.com>
2025-02-14 16:07:44 +0300

    Authentication setup

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,10 +3,14 @@ using Business.BusinessRules;
 using Business.Concrete;
 using Business.DependencyResolvers;
 using Core.CrossCuttingConcerns.Exceptions;
+using Core.Utilities.Security.JWT;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,6 +25,33 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+TokenOptions? tokenOptions = builder.Configuration.GetSection("TokenOptions").Get<TokenOptions>();
+
+builder.Services
+.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+.AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters()
+	{
+		 ValidateIssuer=true,
+		 ValidateAudience=true,
+		ValidIssuer=tokenOptions.Issuer,
+		ValidAudience=tokenOptions.Audience,
+		ValidateLifetime=true,
+		ValidateIssuerSigningKey=true,
+		IssuerSigningKey=new SymmetricSecurityKey
+		(Encoding.UTF8.GetBytes(tokenOptions.SecurityKey))
+
+
+
+	};
+}
+
+
+
+);
+
 var app = builder.Build();
 //if(app.Environment.IsProduction())
 app.UseGlobalExceptionHandling();
@@ -33,6 +64,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
